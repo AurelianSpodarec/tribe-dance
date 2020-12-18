f98ffaa191babc252e2607180075d70eab7583fa
Aurelian Spodarec <aurelianxspodarec@gmail.com>
2020-12-18 23:31:52 +0000

    Updated function files

--- functions.php
+++ functions.php
@@ -26,7 +26,28 @@ require get_stylesheet_directory() . '/includes/scripts-and-styles.php';
 
 if ( ! function_exists( 'tribe_dance_setup' ) ) {
 
+    /**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+
     function tribe_dance_setup() {
+
+        /*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support( 'post-thumbnails' );
+		set_post_thumbnail_size( 1568, 9999 );
+
 
         register_nav_menus(
             array(
@@ -35,8 +56,92 @@ if ( ! function_exists( 'tribe_dance_setup' ) ) {
             )
         );
 
-    }
+
+        add_theme_support( 'title-tag' );
+
+        /**
+		 * Add post-formats support.
+		 */
+		add_theme_support(
+			'post-formats',
+			array(
+				'link',
+				'aside',
+				'gallery',
+				'image',
+				'quote',
+				'status',
+				'video',
+				'audio',
+				'chat',
+			)
+		);
+
+        /*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support(
+			'html5',
+			array(
+				'comment-form',
+				'comment-list',
+				'gallery',
+				'caption',
+				'style',
+				'script',
+				'navigation-widgets',
+			)
+        );
+        
+        /**
+		 * Add support for core custom logo.
+		 *
+		 * @link https://codex.wordpress.org/Theme_Logo
+		 */
+		$logo_width  = 300;
+		$logo_height = 100;
+
+		add_theme_support(
+			'custom-logo',
+			array(
+				'height'               => $logo_height,
+				'width'                => $logo_width,
+				'flex-width'           => true,
+				'flex-height'          => true,
+				'unlink-homepage-logo' => true,
+			)
+		);
+
+    } // tribe_dance_setup
 
 }
 
 add_action( 'after_setup_theme', 'tribe_dance_setup' );
+
+
+
+/**
+ * Register widget area.
+ *
+ * @since 1.0.0
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ *
+ * @return void
+ */
+function twenty_twenty_one_widgets_init() {
+
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Footer', 'twentytwentyone' ),
+			'id'            => 'sidebar-1',
+			'description'   => esc_html__( 'Add widgets here to appear in your footer.', 'twentytwentyone' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+}
+add_action( 'widgets_init', 'twenty_twenty_one_widgets_init' );
--- header.php
+++ header.php
@@ -1,3 +1,10 @@
 <?php get_template_part( 'templates/partials/document-open' ); ?>
 
 <!-- Site header markup goes here -->
+<header>
+
+    Logo
+
+    Menu
+
+</header>
